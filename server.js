b0e7253f4437b6bbba1ec92736b9470a4437f26c
k-okina <okinakahiro@gmail.com>
2017-09-02 09:25:31 +0900

    もうちょっとわかりやすいように修正

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 // ファイルシステムを利用した事をする際に使用するモジュール htmlを返す、templateを使う 等....
 const fs = require('fs');
 
+// テンプレートエンジン
 const ejs = require('ejs');
 
 // serverのip、ポートの値を記載している
@@ -12,11 +13,20 @@ const settings = require('./settings');
 // httpモジュールにhttpサーバーモジュールを作成してもらう
 const server = http.createServer();
 
+// 読み込みファイル
+const file = {
+    // ルート
+    index: {
+        path: __dirname + '/public/index.html',
+        code: 'utf-8',
+    },
+};
+
 // サーバーにリクエストが来た際の処理定義
 // 参考：https://nodejs.org/api/http.html#http_http_createserver_requestlistener
 server.on('request', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data) {
+    fs.readFile(file.index.path, file.index.code, function (err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.write('404 not found!');
@@ -25,22 +35,12 @@ server.on('request', function (req, res) {
 
         // ブラウザはEOFが発されるまでコネクションを繋げている証明を
         res.writeHead(200, {'Content-Type': 'text/html'});
-        let isFinish = false;
-        setTimeout(() => {
-            isFinish = true;
-            res.end();
-        }, 1000);
-
-        const testFunc = () => {
-            if (isFinish) return false;
-            res.write(data);
-            setTimeout(testFunc, 100);
-        };
-        testFunc();
+        res.end(data);
     });
 });
 
 // サーバー起動
 server.listen(settings.port, settings.host);
 // 起動したと通知
+console.log('IP: ' + settings.host + ':' + settings.port);
 console.log('server listening...');
